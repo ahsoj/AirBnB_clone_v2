ee47102a4649c8b16d6752c92fe9ef7dd0f0281c
eyasu chaka <daa282386@gmail.com>
2023-04-06 18:14:35 +0000

    deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ import os
 #import logging
 from datetime import datetime
 
-env.hosts = ['52.91.136.103', '18.235.248.251']
+env.hosts = ['18.235.248.251', '52.91.136.103']
 env.sudo_prefix = "sudo -S '%(sudo_prompt)s' " % env
 
 #logging.basicConfig(level=logging.INFO)
@@ -27,13 +27,13 @@ def do_deploy(archive_path):
     base_n = os.path.basename(archive_path)
     try:
         put(archive_path, '/tmp/{}'.format(base_n))
-        run("mkdir -p {}/".format(ld))
-        run("tar -xzf /tmp/{} -C {}/".format(base_n, ld))
-        run("rm -rf /tmp/{}".format(base_n))
-        run("mv {}/web_static/* {}/".format(ld, ld))
-        run("rm -rf {}/web_static".format(ld))
-        run("rm -rf /data/web_static/current")
-        run("ln -fs {}/ /data/web_static/current".format(ld))
+        run("sudo mkdir -p {}/".format(ld))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(base_n, ld))
+        run("sudo rm -rf /tmp/{}".format(base_n))
+        run("sudo mv {}/web_static/* {}/".format(ld, ld))
+        run("sudo rm -rf {}/web_static".format(ld))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -fs {}/ /data/web_static/current".format(ld))
         print('New version deployed!')
     except Exception:
         return False
