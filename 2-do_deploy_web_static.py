d8dd0da021b8360bb16a49a9ba5174310a2e8452
eyasu chaka <daa282386@gmail.com>
2023-04-06 18:17:10 +0000

    deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,13 +27,13 @@ def do_deploy(archive_path):
     base_n = os.path.basename(archive_path)
     try:
         put(archive_path, '/tmp/{}'.format(base_n))
-        run("sudo mkdir -p {}/".format(ld))
-        run("sudo tar -xzf /tmp/{} -C {}/".format(base_n, ld))
-        run("sudo rm -rf /tmp/{}".format(base_n))
-        run("sudo mv {}/web_static/* {}/".format(ld, ld))
-        run("sudo rm -rf {}/web_static".format(ld))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -fs {}/ /data/web_static/current".format(ld))
+        run("mkdir -p {}/".format(ld))
+        run("tar -xzf /tmp/{} -C {}/".format(base_n, ld))
+        run("rm -rf /tmp/{}".format(base_n))
+        run("mv {}/web_static/* {}/".format(ld, ld))
+        run("rm -rf {}/web_static".format(ld))
+        run("rm -rf /data/web_static/current")
+        run("ln -fs {}/ /data/web_static/current".format(ld))
         print('New version deployed!')
     except Exception:
         return False
