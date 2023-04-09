6cb65428d9cf8fbfd4bd922b24f4db416563537f
eyasu chaka <daa282386@gmail.com>
2023-04-09 12:41:44 +0000

    do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,6 +32,7 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -fs {}/ /data/web_static/current".format(ld))
         print('New version deployed!')
+        return True
     except Exception:
         return False
 
