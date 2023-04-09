aab61e7cbc3f837d440bbd95b6620f7ed8f59a48
eyasu chaka <daa282386@gmail.com>
2023-04-09 12:56:55 +0000

    do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-   Fabric script (based on the file 1-pack_web_static.py) \
-	 that distributes an archive to your web servers
-"""
+
 from fabric.api import put, env, run, runs_once, sudo
 import os
 from datetime import datetime
 
 env.hosts = ['18.235.248.251', '52.91.136.103']
-# env.sudo_prefix = "sudo -S '%(sudo_prompt)s' " % env
+env.sudo_prefix = "sudo -S '%(sudo_prompt)s' " % env
 
 
 def do_deploy(archive_path):
@@ -23,14 +20,17 @@ def do_deploy(archive_path):
     ld = "/data/web_static/releases/{}".format(dirs)
     base_n = os.path.basename(archive_path)
 
-    put(archive_path, '/tmp/{}'.format(base_n))
-    run("mkdir -p {}/".format(ld))
-    run("tar -xzf /tmp/{} -C {}/".format(base_n, ld))
-    run("rm -rf /tmp/{}".format(base_n))
-    run("mv {}/web_static/* {}/".format(ld, ld))
-    run("rm -rf {}/web_static".format(ld))
-    run("rm -rf /data/web_static/current")
-    run("ln -fs {}/ /data/web_static/current".format(ld))
-    print('New version deployed!')
-    return True
+    try:
+        put(archive_path, '/tmp/{}'.format(base_n))
+        run("mkdir -p {}/".format(ld))
+        run("tar -xzf /tmp/{} -C {}/".format(base_n, ld))
+        run("rm -rf /tmp/{}".format(base_n))
+        run("mv {}/web_static/* {}/".format(ld, ld))
+        run("rm -rf {}/web_static".format(ld))
+        run("rm -rf /data/web_static/current")
+        run("ln -fs {}/ /data/web_static/current".format(ld))
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
 
