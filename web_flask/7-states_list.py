6df0ac4d8b31ecb64fa871ad97bdd51294b13770
eyasu chaka <daa282386@gmail.com>
2023-04-21 10:54:08 -0700

    use_db_storage

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,14 +4,14 @@
 from models import storage
 from flask import Flask, render_template
 
+# storage = __import__("../models").storage
+
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
+    """Displays an HTML page with a list of all State objects"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
