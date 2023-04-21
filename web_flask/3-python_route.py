a92f2839c7455b3ba1d2c7ce814205aa47070625
eyasu chaka <daa282386@gmail.com>
2023-04-21 09:30:33 -0700

    optional_route_path

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Create Flask Base app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Index page of web page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """path /hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cFun(text):
+    """route with argument"""
+    rep_text = text.replace('_', ' ')
+    return "C {}".format(rep_text)
+
+
+@app.route("/python)", strict_slashes=False)
+@app.route("/python/(<text>)", strict_slashes=False)
+def pythonFun(text="is cool"):
+    """route with argument"""
+    rep_text = text.replace('_', ' ')
+    return "Python {}".format(rep_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
